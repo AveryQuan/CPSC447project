baffd6b59637bd5da932db4389b5b65d3a6b7d6a
Pradyut Mandal <pradmandal@dhcp-128-189-136-11.ubcsecure.wireless.ubc.ca>
2023-11-19 20:40:55 -0800

    added genre sorting

--- js/main.js
+++ js/main.js
@@ -1,39 +1,36 @@
-
 let data, scatterPlotVis, treemapVis, votesScorePlotVis;
 
 const dispatcher = d3.dispatch('event1', 'event2'); // Replace events with event names
 
-d3.csv("data/movies.csv") 
+d3.csv("data/movies.csv")
   .then(_data => {
-      console.log(_data)
     data = preprocessData(_data);
 
-    console.log(data)
-      scatterPlotVis = new ScatterPlot({
-          parentElement: '#scatterPlotDiv',
-          xValue: d => d.gross,
-          yValue: d => d.score,
-          title: "Movie Score vs Revenue",
-          xTitle: "Movie Revenue (in Billions)",
-          yTitle: "Movie Score",
-          xTick: " B"
-        }, data);
-      scatterPlotVis.updateVis();
+    scatterPlotVis = new ScatterPlot({
+        parentElement: '#scatterPlotDiv',
+        xValue: d => d.gross,
+        yValue: d => d.score,
+        title: "Movie Score vs Revenue",
+        xTitle: "Movie Revenue (in Billions)",
+        yTitle: "Movie Score",
+        xTick: " B"
+      }, data);
+    scatterPlotVis.updateVis();
 
-      votesScorePlotVis = new ScatterPlot({
-          parentElement: '#votesScorePlotDiv',
-          xValue: d => d.votes,
-          yValue: d => d.score,
-          title: "Number of Votes vs Movie Score",
-          xTitle: "Number of Votes (in Millions)",
-          yTitle: "Movie Score",
-          xTick: " M"
-        }, data);
-      votesScorePlotVis.updateVis();
+    votesScorePlotVis = new ScatterPlot({
+        parentElement: '#votesScorePlotDiv',
+        xValue: d => d.votes,
+        yValue: d => d.score,
+        title: "Number of Votes vs Movie Score",
+        xTitle: "Number of Votes (in Millions)",
+        yTitle: "Movie Score",
+        xTick: " M"
+      }, data);
+    votesScorePlotVis.updateVis();
 
     // treemapVis = new Treemap({
     //   parentElement: '#treemapDiv',
-    //   //other config
+    //   // other configuration
     // }, data);
     // treemapVis.updateVis();
 
@@ -41,18 +38,33 @@ d3.csv("data/movies.csv")
   .catch(error => console.error('Error loading the dataset:', error));
 
 function preprocessData(_data) {
-    console.log(_data)
     _data.forEach(d => {
         d.year = +d.year;
         d.score = +d.score;
-        d.votes = Math.abs(Number(+d.votes)) / 1.0e+6;
-        d.gross = Math.abs(Number(+d.gross)) / 1.0e+9;
+        d.votes = Math.abs(Number(d.votes)) / 1.0e+6;
+        d.gross = Math.abs(Number(d.gross)) / 1.0e+9;
     });
-    console.log(_data)
-    return _data.filter(d => d.year >= 2010);
+
+    _data = _data.filter(d => d.year >= 2010);
+
+    const genreFrequency = d3.rollup(_data, v => v.length, d => d.genre);
+
+    console.log("Genre Frequencies:", genreFrequency);
+
+
+    let genreToFrequencyMap = new Map(genreFrequency);
+
+    _data.sort((a, b) => {
+        let genreAFrequency = genreToFrequencyMap.get(a.genre) || 0;
+        let genreBFrequency = genreToFrequencyMap.get(b.genre) || 0;
+        return genreBFrequency - genreAFrequency; 
+    });
+    console.log("First few sorted records:", _data.slice(0, 10));
+
+    return _data;
 }
 
 dispatcher.on('event1', function(someParameter) {
     // scatterPlotVis.updateVis();
-    //etc.
+    // etc.
 });
