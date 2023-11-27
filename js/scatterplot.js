5c1392baa465faca15157a62ce8c327561287c2b
aquan01 <aquan01@students.cs.ubc.ca>
2023-11-26 22:30:14 -0800

    add brush

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -9,8 +9,10 @@ class ScatterPlot {
     this.config = {
       parentElement: _config.parentElement,
       containerWidth: 1000,
-      containerHeight: 400,
-      margin: {top: 75, right: 20, bottom: 20, left: 35},
+      containerHeight: 600,
+      margin: {top: 75, right: 20, bottom: 70, left: 35},
+      contextMargin: {top: 500, right: 10, bottom: 20, left: 35},
+      contextHeight: 70,
       tooltipPadding: 15,
       xValue: _config.xValue,
       yValue: _config.yValue,
@@ -29,10 +31,18 @@ class ScatterPlot {
   initVis() {
     let vis = this;
 
+    // Calculate inner chart size.console Margin specifies the space around the actual chart.
+    vis.config.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
+    vis.config.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom - vis.config.contextHeight;
+
     // Initialize scales
 
     vis.xScale = d3.scaleLinear();
     vis.yScale = d3.scaleLinear();
+    vis.xScaleBrush = d3.scaleLinear()
+        .range([0, vis.config.width]);
+    vis.yScaleBrush = d3.scaleLinear()
+        .range([vis.config.contextHeight, 0]);
 
     // Initialize axes
     vis.xAxis = d3.axisBottom(vis.xScale)
@@ -47,9 +57,14 @@ class ScatterPlot {
     // Define size of SVG drawing area
     vis.svg = d3.select(vis.config.parentElement).append('svg');
 
+    // Append focus group with x- and y-axes
+    vis.brushGroup = vis.svg.append('g')
+        .attr('transform', `translate(${vis.config.contextMargin.left},${vis.config.contextMargin.top})`);
+
     // Append group element that will contain our actual chart
     // and position it according to the given margin config
     vis.chart = vis.svg.append('g')
+        .attr('height', 200)
         .attr('transform', `translate(${vis.config.margin.left},${vis.config.margin.top})`);
 
     // Append empty x-axis group and move it to the bottom of the chart
@@ -60,6 +75,12 @@ class ScatterPlot {
     vis.yAxisG = vis.chart.append('g')
         .attr('y', 100)
         .attr('class', 'axis y-axis');
+
+    vis.xAxisBrush = d3.axisBottom(vis.xScaleBrush)
+        .tickSizeOuter(0)
+        .ticks(6)
+        .tickPadding(10)
+        .tickFormat(d => d + vis.config.xTick);;
 
     vis.title = vis.svg.append('text')
         .attr('class', 'chart-title')
@@ -82,6 +103,25 @@ class ScatterPlot {
         .attr('y', -20)
         .attr('dy', '.71em')
         .text(vis.config.yTitle);
+
+
+    vis.xAxisBrushG = vis.brushGroup.append('g')
+        .attr('class', 'axis x-axis')
+        .attr('transform', `translate(0,${vis.config.contextHeight})`);
+
+    vis.brushG = vis.brushGroup.append('g')
+        .attr('class', 'brush x-brush');
+
+    // Initialize brush component
+    vis.brush = d3.brushX()
+        .extent([[0, 0], [vis.config.width, vis.config.contextHeight]])
+        .on('brush', function({selection}) {
+          if (selection) vis.brushed(selection);
+        })
+        .on('end', function({selection}) {
+          if (!selection) vis.brushed(null);
+        });
+
   }
 
   /**
@@ -89,10 +129,6 @@ class ScatterPlot {
    */
   updateVis() {
     let vis = this;
-
-    // Calculate inner chart size. Margin specifies the space around the actual chart.
-    vis.config.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
-    vis.config.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom;
 
     vis.svg
         .attr('width', vis.config.containerWidth)
@@ -120,6 +156,11 @@ class ScatterPlot {
         .range([vis.config.height, 0])
         .domain([0, d3.max(vis.data, vis.config.yValue)]);
 
+    vis.xScaleBrush
+        .domain([0, d3.max(vis.data, vis.config.xValue)]);
+    vis.yScaleBrush
+        .domain([0, d3.max(vis.data, vis.config.yValue)]);
+
     vis.renderVis();
   }
 
@@ -130,7 +171,7 @@ class ScatterPlot {
     let vis = this;
 
     // Add circles
-    const circles = vis.chart.selectAll('.point')
+    vis.circles = vis.chart.selectAll('.point')
         .data(vis.data, d => d.trail)
       .join('circle')
         .attr('class', 'point')
@@ -139,7 +180,7 @@ class ScatterPlot {
         .attr('cx', d => vis.xScale(vis.config.xValue(d)));
 
     // Tooltip event listeners
-    circles
+    vis.circles
         .on('mouseover', (event,d) => {
           d3.select('#tooltip')
             .style('display', 'block')
@@ -160,6 +201,14 @@ class ScatterPlot {
           d3.select('#tooltip').style('display', 'none');
         });
 
+    const circlesBrush = vis.brushGroup.selectAll('.point')
+        .data(vis.data, d => d.trail)
+        .join('circle')
+        .attr('class', 'point')
+        .attr('r', 2)
+        .attr('cy', d => vis.yScaleBrush(vis.config.yValue(d)))
+        .attr('cx', d => vis.xScaleBrush(vis.config.xValue(d)));
+
     // Update the axes/gridlines
     // We use the second .call() to remove the axis and just show gridlines
     vis.xAxisG
@@ -169,5 +218,39 @@ class ScatterPlot {
     vis.yAxisG
         .call(vis.yAxis)
         .call(g => g.select('.domain').remove())
+
+    vis.xAxisBrushG.call(vis.xAxisBrush);
+
+    // Update the brush and define a default position
+    const defaultBrushSelection = [vis.xScale(0), vis.xScaleBrush.range()[1]];
+    vis.brushG
+        .call(vis.brush)
+        .call(vis.brush.move, defaultBrushSelection);
+  }
+
+  /**
+   * React to brush events
+   */
+  brushed(selection) {
+    let vis = this;
+
+    // Check if the brush is still active or if it has been removed
+    if (selection) {
+      // Convert given pixel coordinates (range: [x0,x1]) into a time period (domain: [Date, Date])
+      const selectedDomain = selection.map(vis.xScaleBrush.invert, vis.xScaleBrush);
+
+      // Update x-scale of the focus view accordingly
+      vis.xScale.domain(selectedDomain);
+    } else {
+      // Reset x-scale of the focus view (full time period)
+      vis.xScale.domain(vis.xScaleBrush.domain());
+    }
+
+    // Redraw circles and update x-axis labels in focus view
+
+    vis.circles
+        .attr('cy', d => vis.yScale(vis.config.yValue(d)))
+        .attr('cx', d => vis.xScale(vis.config.xValue(d)));
+    vis.xAxisG.call(vis.xAxis);
   }
 }
